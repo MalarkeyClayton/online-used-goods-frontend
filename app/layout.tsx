5b6d3af92ef4054dfa181b55ad7813ad3417087e
happydev-cyber <happylife827222@gmail.com>
2025-02-03 08:29:34 +0900

    fix(module): fix module error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import TopNavbar from "@/components/layout/Navbar/TopNavbar";
 import Footer from "@/components/layout/Footer";
 import HolyLoader from "holy-loader";
 import Providers from "./providers";
+import Sign from "./sign/page";
 
 export const metadata: Metadata = {
   title: "Shopco",
@@ -16,6 +17,8 @@ export const viewport: Viewport = {
   themeColor: "#000000",
 };
 
+const token = {};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,13 +27,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={satoshi.className}>
-        <HolyLoader color="#868686" />
-        <TopBanner />
-        <Providers>
-          <TopNavbar />
-          {children}
-        </Providers>
-        <Footer />
+        {token &&
+          token ? (
+          <Sign />
+        ) : (
+          <>
+            <HolyLoader color="#868686" />
+            <TopBanner />
+            <Providers>
+              <TopNavbar />
+              {children}
+            </Providers>
+            <Footer />
+          </>
+        )
+        }
       </body>
     </html>
   );
