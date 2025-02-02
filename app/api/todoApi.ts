294ede3a296da03b121ab8414741b166c573ee58
happydev-cyber <happylife827222@gmail.com>
2025-02-02 09:42:47 +0900

    feat(ath): add signin and signup functionality

--- app/api/todoApi.ts
+++ app/api/todoApi.ts
@@ -1,26 +1,7 @@
-import { Todo } from "@/types/type";
-import axios from "axios";
-
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8001";
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8001";
 
 const todoApi = {
-    getTodos: async () => {
-        console.log(baseUrl);
-        const response = await axios.get(`${baseUrl}/test`);
-        return response.data;
-    },
-    createTodo: async (todo: Todo) => {
-        const response = await axios.post(`${baseUrl}/test`, todo);
-        return response.data;
-    },
-    updateTodo: async (todo: Todo) => {
-        const response = await axios.put(`${baseUrl}/test/${todo._id}`, todo);
-        return response.data;
-    },
-    deleteTodo: async (id: string) => {
-        const response = await axios.delete(`${baseUrl}/test/${id}`);
-        return response.data;
-    }
+    
 };
 
 export default todoApi;
